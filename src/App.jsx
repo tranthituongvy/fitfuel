e64939a167e7ebec1d17378718c74d1be8ec6add
vytran <tranthituongvy.qn@gmail.com>
2026-02-25 09:14:16 +0700

    Day 3: create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Navbar />    
+      <Navbar />
+
+      <Home />    
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+    return (
+        <div style={styles.container}>
+            <h2>Fuel Your Strength</h2>
+            <p>Healthy meals for a stronger, better you.</p>
+        </div>
+    )
+}
+
+const styles = {
+    container: {
+        padding: "40px",
+        textAlign: "center"
+    }
+}
+
+export default Home;
