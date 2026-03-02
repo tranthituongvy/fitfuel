c39a6003127891785ecd3b7726d32544eb1cb4c7
vytran <tranthituongvy.qn@gmail.com>
2026-03-02 13:41:21 +0700

    add a FoodCard

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,6 +1,7 @@
-function FoodCard({ name, calories }) {
+function FoodCard({ name, calories, onClick }) {   
+
     return (
-        <div>
+        <div onClick={onClick}>
             <h3>{name}</h3>
             <p>{calories} cal</p>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,23 @@ function Home() {
 
         setFoods([...foods, newFood]);
     }
+
+    const increaseCalories = (id) => {
+        console.log(id)
+        const updateFoods = foods.map(food => {
+            if(food.id === id) {
+                return {
+                    ...food,
+                    calories: food.calories + 10
+                };
+            }
+            return food;
+        });
+
+        setFoods(updateFoods);
+    }
+
+    
     return (
         <div className="home-container">
             <button onClick={addFood}>Add Food</button>  
@@ -25,6 +42,7 @@ function Home() {
                     key={food.id}
                     name={food.name}
                     calories={food.calories}
+                    onClick={() => increaseCalories(food.id)}
                 />
 
             ))}           
