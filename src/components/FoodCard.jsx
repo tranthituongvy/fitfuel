5fbee2864f16d57ba157670462c33d5b1d8ae4af
Vy Tran <tranthituongvy.qn@gmail.com>
2026-03-07 16:56:11 +0700

    Way 2 - FoodCard call FoodContext

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,6 +1,20 @@
-function FoodCard({ food, onIncrease }) {
+import { useContext } from "react";
+import { FoodContext } from "../context/FoodContext";
+
+//function FoodCard({ food, onIncrease }) {
+  //return (
+    //<div onClick={() => onIncrease(food.id)}>
+      //<h3>{food.name}</h3>
+      //<p>{food.calories} cal</p>
+    //</div>
+  //);  
+//}
+
+function FoodCard({food}) {
+  const { increaseCalories } = useContext(FoodContext);
+
   return (
-    <div onClick={() => onIncrease(food.id)}>
+    <div onClick={() => increaseCalories(food.id)}>
       <h3>{food.name}</h3>
       <p>{food.calories} cal</p>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,35 @@ import { useContext } from "react";
 import { FoodContext } from "../context/FoodContext";
 import FoodCard from "../components/FoodCard";
 
-function Home() {
-   const { foods, addFood, increaseCalories } = useContext(FoodContext);
+//function Home() {
+   //const { foods, addFood, increaseCalories } = useContext(FoodContext);
 
-   return (
-    <div className="home-container">
+   //return (
+    //<div className="home-container">
+      //<button onClick={addFood}>Add Food</button>
+
+      //{foods.map(food => (
+        //<FoodCard
+         // key={food.id}
+          //food={food}
+          //onIncrease={increaseCalories}
+        ///>
+      //))}
+    //</div>
+   //);
+//}
+
+function Home() {
+  const { foods, addFood} = useContext(FoodContext);
+
+  return (    
+    <div className="home-container">  
       <button onClick={addFood}>Add Food</button>
 
       {foods.map(food => (
         <FoodCard
           key={food.id}
           food={food}
-          onIncrease={increaseCalories}
         />
       ))}
     </div>
