75407ee7afd8c813c5f7a7b25562f10f6500bf6d
vytran <tranthituongvy.qn@gmail.com>
2026-03-12 14:16:19 +0700

    Day 12 - Add field search Food

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FoodContext } from "../context/FoodContext";
 import FoodCard from "../components/FoodCard";
 
 function Home() {
    const { foods, addFood, increaseCalories, deleteFood, updateCalories } = useContext(FoodContext);
+   const [search, setSearch] = useState("");
+
+   const filteredFoods = foods.filter(food =>
+    food.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
 
    return (
     <div className="home-container">
+      <input
+        type="text"
+        placeholder="Search food..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      /> 
+
       <button onClick={addFood}>Add Food</button>
 
-      {foods.map(food => (
+      {filteredFoods.map(food => (
         <FoodCard
           key={food.id}
           food={food}
